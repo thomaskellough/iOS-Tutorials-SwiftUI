057c8dd477e0107530d2aa1bcaa8719ab4af38e7
thomas.kellough <thomas.kellough@brandtinfo.com>
2021-04-02 18:57:06 -0600

    init commit

---
+++ How-To-Use-Core-Location/How-To-Use-Core-Location/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  How-To-Use-Core-Location
+//
+//  Created by Thomas Kellough on 4/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var locationRetrieved = false
+    @State private var addressRetrieved = false
+    @State private var coordinates: String = ""
+    @State private var address: String = ""
+    private let coreLocationManager = CoreLocationManager()
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Button("Show location") {
+                determineLocation()
+            }
+            
+            if locationRetrieved {
+                Text("Your coordinates are: \(coordinates)")
+            }
+            
+            Button("Show address") {
+                showAddress()
+            }
+            
+            if addressRetrieved {
+                Text("Your address is: \(address)")
+            }
+        }
+    }
+    
+    func determineLocation() {
+        guard let location = coreLocationManager.getCurrentLocation() else {
+            locationRetrieved = false
+            return
+        }
+        let latitude = location.latitude
+        let longitude = location.longitude
+        coordinates = "\(latitude), \(longitude)"
+        locationRetrieved = true
+    }
+    
+    func showAddress() {
+        coreLocationManager.getAddress { returnedAddress in
+            guard let unwrappedAddress = returnedAddress else {
+                addressRetrieved = false
+                return
+            }
+
+            address = unwrappedAddress
+            addressRetrieved = true
+            
+        }
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ How-To-Use-Core-Location/How-To-Use-Core-Location/CoreLocationManager.swift
@@ -0,0 +1,54 @@
+//
+//  CoreLocationManager.swift
+//  How-To-Use-Core-Location
+//
+//  Created by Thomas Kellough on 4/2/21.
+//
+
+import CoreLocation
+
+class CoreLocationManager: NSObject, CLLocationManagerDelegate {
+    private var locationManager = CLLocationManager()
+    
+    func determineCurrentLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func getCurrentLocation() -> CLLocationCoordinate2D? {
+        determineCurrentLocation()
+        
+        return locationManager.location?.coordinate
+    }
+    
+    func getAddress(completion: @escaping (String?) -> Void) {
+        guard let coordinates = getCurrentLocation() else { return }
+        let location = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { placemarks, error in
+            if error != nil {
+                print("Error: \(error)")
+                completion(nil)
+            }
+            
+            if placemarks != nil {
+                guard let first = placemarks?.first else {
+                    completion(nil)
+                    return
+                }
+                
+                guard let street = first.name else { return }
+                guard let city = first.locality else { return }
+                guard let state = first.administrativeArea else { return }
+                
+                completion("\(street) \(city), \(state)")
+            }
+        }
+    }
+}
